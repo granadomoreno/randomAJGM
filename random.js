9d487fd9fd53a13259a5b7d12734b2b05de6b585
Antonio Jesús Granado Moreno <granadomoreno@gmail.com>
2015-06-09 06:16:35 +0200

    Random version 2

--- random.js
+++ random.js
@@ -1,7 +1,9 @@
 var numero = Match.random();
 
+var str = ' MAYOR que 0.5';
+
 if (numero<=0.5){
-  console.log('\n'+numero+' MENOR que 0.5\n');
-}else{
-  console.log('\n'+numero+' MAYOR que 0.5\n');
+  str = ' MENOR que 0.5\n');
 }
+
+console.log('\n'+numero+str+'\n');
